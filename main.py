df7557e3f82b89b4d7f0918a9a85d9ae85bf5d2c
badinjetslux <momorobot@gmail.com>
2025-05-27 10:49:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
 import mysql.connector
 import os
 
 # Connessione al database su Hostinger
 connection = mysql.connector.connect(
-    host="srv1222.hstgr.io",
+    host="auth-db1579.hstgr.io",
     user="u536233056_badinjetluxapp",
     password="Momobady1989",
     database="u536233056_badinjetluxapp"
