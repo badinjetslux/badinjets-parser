e01fbca2d8be37286d11f34d454ee3f0cb267371
badinjetslux <momorobot@gmail.com>
2025-05-30 11:21:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import mysql.connector
 
 connection = mysql.connector.connect(
-    host="mysql.hostinger.com",  # ⚠️ Sostituisci con l'host corretto se diverso (es:  `srvxxx.main-hosting.eu`)
+    host="srv123.main-hosting.eu",  # <-- metti qui l'host corretto che vedi su Hostinger
     user="u536233056_badinjetluxapp",
     password="Momobady1989",
-    database="u536233056_badinjetluxapp"
-)
+    database="u536233056_badinjetluxapp")
 
 cursor = connection.cursor()
 cursor.execute("SHOW TABLES")
